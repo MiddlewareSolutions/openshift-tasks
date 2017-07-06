4fd7b1fe3f51592606dcd5b1e9641012e5b9700c
Emmanuel <emmanuel.lesne@middleware-solutions.fr>
2017-07-06 22:29:56 +0200

    some corrections

---
+++ src/test/java/org/jboss/as/quickstarts/tasksrs/SmokeIntegrationIT.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.tasksrs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SmokeIntegrationIT {
+
+	@Test
+	public void smoke() {
+		assertTrue(true);
+	}
+}
